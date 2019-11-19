da8c73e1c0bf47c376de92caa5799608a4f581fe
chrissa <chris.sams@dat.com>
2019-11-18 20:07:59 -0800

    Made some changes to the Mandelbrot set

--- src/app/mandelbrot/mandelbrot.component.ts
+++ src/app/mandelbrot/mandelbrot.component.ts
@@ -7,8 +7,6 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./mandelbrot.component.scss']
 })
 export class MandelbrotComponent implements OnInit {
-  setA: [number] = [];
-  setB: [number] = [];
 
   @ViewChild('mandelbrotCanvas', { static: false }) mandelbrotCanvas: ElementRef;
   public context: CanvasRenderingContext2D;
@@ -16,18 +14,17 @@ export class MandelbrotComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    for (let x = 0; x < 600; x++) {
-        this.setA.push(2 / x);
-        this.setB.push(-2 / x);
-    }
   }
 
-  caluclate(): void {
+  calculate(red, green, blue): void {
+
     for (let x = 0; x < 600; x++) {
       for (let y = 0; y < 600; y++) {
         let i = 0;
-        const cx = -2.5 + x / 50;
-        const cy = -2.5 + y / 50;
+        // tslint:disable-next-line:radix
+        const cx = -2.5 + x / 120;
+        // tslint:disable-next-line:radix
+        const cy = -2.5 + y / 120;
         let zx = 0;
         let zy = 0;
 
@@ -42,7 +39,7 @@ export class MandelbrotComponent implements OnInit {
         const color = i.toString(16);
         this.context.beginPath();
         this.context.rect(x, y, 1, 1);
-        this.context.fillStyle = '#' + color + color + color;
+        this.context.fillStyle = '#' + color + '00' + color;
         this.context.fill();
       }
     }
@@ -56,6 +53,6 @@ export class MandelbrotComponent implements OnInit {
   // tslint:disable-next-line:use-lifecycle-interface
   ngAfterViewInit(): void {
     this.context = this.mandelbrotCanvas.nativeElement.getContext('2d');
-    this.caluclate();
+    this.calculate(0, 0, 0);
   }
 }
