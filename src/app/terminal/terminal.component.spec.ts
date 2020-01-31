71dfce04ed719f34908b0ae63b6f7c6e7e681b43
chrissa <chris.sams@dat.com>
2020-01-30 20:52:31 -0800

    Minor cleanup

--- src/app/terminal/terminal.component.spec.ts
+++ src/app/terminal/terminal.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TerminalComponent } from './terminal.component';
-import {isArray} from "util";
+import { isArray } from 'util';
 
 describe('TerminalComponent', () => {
   let component: TerminalComponent;
